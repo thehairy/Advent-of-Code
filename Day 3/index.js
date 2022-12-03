c52322d77c1db6fc90e937008082d2d27d168331
thehairy <soeren.stabenow@outlook.com>
2022-12-03 13:56:29 +0100

    Add debug time

--- Day 3/index.js
+++ Day 3/index.js
@@ -1,7 +1,10 @@
+const { performance } = require('perf_hooks');
 module.exports = {
     run: () => {
-        const partOne = require('./Part 1/index');
-        const partTwo = require('./Part 2/index');
-        return { partOne: partOne.run(), partTwo: partTwo.run() }
+        const startTime = performance.now()
+        const partOne = require('./Part 1/index').run();
+        const partTwo = require('./Part 2/index').run();
+        const endTime = performance.now()
+        return { partOne: partOne, partTwo: partTwo, took: endTime - startTime }
     }
 }
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dayTwo = require('./Day 2/index').run();
 const dayThree = require('./Day 3/index').run();
 
 const data = [
-    ['Day', 'Description', 'Part 1', 'Part 2']
+    ['Day', 'Description', 'Part 1', 'Part 2', 'Took']
 ];
 
 const config = {
@@ -15,6 +15,7 @@ const config = {
         { alignment: 'center' },
         { alignment: 'center', width: 23 },
         { alignment: 'center' },
+        { alignment: 'center' },
         { alignment: 'center' }
     ],
     header: {
@@ -23,9 +24,9 @@ const config = {
     },
 }
 
-data.push(['1', 'Calorie Counting', dayOne.partOne, dayOne.partTwo]);
-data.push(['2', 'Rock Paper Scissors', dayTwo.partOne, dayTwo.partTwo]);
-data.push(['3', 'Rucksack Reorganization', dayThree.partOne, dayThree.partTwo]);
+data.push(['1', 'Calorie Counting', dayOne.partOne, dayOne.partTwo, Number(dayOne.took).toFixed(2) + 'ms']);
+data.push(['2', 'Rock Paper Scissors', dayTwo.partOne, dayTwo.partTwo, Number(dayTwo.took).toFixed(2) + 'ms']);
+data.push(['3', 'Rucksack Reorganization', dayThree.partOne, dayThree.partTwo, Number(dayThree.took).toFixed(2) + 'ms']);
 
 
 console.log(table(data, config));
