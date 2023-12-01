d3a2d213096e5f551e32b7bea72f46a3c9b42858
Stabenow <soestabenow@fiege.com>
2023-12-01 14:31:02 +0100

    day 1 complete

--- 2023/Day 1/Part 2/index.js
+++ 2023/Day 1/Part 2/index.js
@@ -1,10 +1,36 @@
 const path = require("path");
 const fs = require('fs');
 
+function extractNumbers(str) {
+    // Regex pattern for digits and spelled-out numbers from one to nine
+    const pattern = /one|two|three|four|five|six|seven|eight|nine|\d/gi;
+    
+    // Find all non-overlapping matches
+    let matches = str.match(pattern);
+    if (!matches) {
+        return []; // No matches found
+    }
+
+    // Iterate over the string to find overlapping matches
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; j <= str.length; j++) {
+            let substr = str.substring(i, j);
+            let match = substr.match(pattern);
+            if (match && match[0] !== substr) {
+                matches.push(match[0]);
+            }
+        }
+    }
+
+    // Remove duplicates from the array
+    matches = [...new Set(matches)];
+    return matches
+}
+
 module.exports = {
     run: () => {
         const i = fs.readFileSync(path.resolve(__dirname, "../input.txt")).toString().split('\r\n');
-
+        
         const o = {
             'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 
             'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
@@ -12,14 +38,8 @@ module.exports = {
 
         let s = 0;
         for (const l of i) {
-            const nol = [];
-            for (const c of l.replace(/(one|two|three|four|five|six|seven|eight|nine)/gi, m => o[m])) {
-                if (!isNaN(c)) {
-                    nol.push(c);
-                }
-            }
-            const ls = nol[0] + nol[nol.length - 1];
-            s += +ls;
+            const n = [...l.matchAll(/(?=(one|two|three|four|five|six|seven|eight|nine|\d))/gi)].map(j => !isNaN(j[1]) ? j[1] : o[j[1]]);
+            s += +(n[0] + n[n.length - 1]);
         }
 
         return s;
