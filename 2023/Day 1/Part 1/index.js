17d5cbe36a64e21f8a2d4d5491d597dadc83f542
Stabenow <soestabenow@fiege.com>
2023-12-01 16:43:47 +0100

    performance improvement

--- 2023/Day 1/Part 1/index.js
+++ 2023/Day 1/Part 1/index.js
@@ -1,22 +1,13 @@
 const path = require("path");
 const fs = require('fs');
 
+const input = fs.readFileSync(path.resolve(__dirname, "../input.txt")).toString().split('\r\n')
+
 module.exports = {
     run: () => {
-        const input = fs.readFileSync(path.resolve(__dirname, "../input.txt")).toString().split('\r\n');
-
-        let sum = 0;
-        for (const line of input) {
-            const numbersOfLine = [];
-            for (const char of line) {
-                if (!isNaN(char)) {
-                    numbersOfLine.push(char);
-                }
-            }
-            const lineSum = numbersOfLine[0] + '' + numbersOfLine[numbersOfLine.length - 1];
-            sum += +lineSum;
-        }
-
-        return sum;
+        return input.map(l => {
+                const line = l.match(/\d/g);
+                return line[0] + line[line.length-1];
+            }).reduce((a, c) => +a + +c, 0);
     }
 }
--- 2023/Day 1/Part 2/index.js
+++ 2023/Day 1/Part 2/index.js
@@ -1,36 +1,10 @@
 const path = require("path");
 const fs = require('fs');
 
-function extractNumbers(str) {
-    // Regex pattern for digits and spelled-out numbers from one to nine
-    const pattern = /one|two|three|four|five|six|seven|eight|nine|\d/gi;
-    
-    // Find all non-overlapping matches
-    let matches = str.match(pattern);
-    if (!matches) {
-        return []; // No matches found
-    }
-
-    // Iterate over the string to find overlapping matches
-    for (let i = 0; i < str.length; i++) {
-        for (let j = i + 1; j <= str.length; j++) {
-            let substr = str.substring(i, j);
-            let match = substr.match(pattern);
-            if (match && match[0] !== substr) {
-                matches.push(match[0]);
-            }
-        }
-    }
-
-    // Remove duplicates from the array
-    matches = [...new Set(matches)];
-    return matches
-}
+const i = fs.readFileSync(path.resolve(__dirname, "../input.txt")).toString().split('\r\n')
 
 module.exports = {
     run: () => {
-        const i = fs.readFileSync(path.resolve(__dirname, "../input.txt")).toString().split('\r\n');
-        
         const o = {
             'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 
             'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
