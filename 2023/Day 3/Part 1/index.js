c7e3247293e2b01743fc285d33782b577eedfbc0
thehairy <soeren.stabenow@outlook.com>
2023-12-04 10:17:05 +0100

    useless fancy stuff

--- 2023/Day 3/Part 1/index.js
+++ 2023/Day 3/Part 1/index.js
@@ -1,13 +1,10 @@
 const path = require("path");
 const fs = require('fs');
 
-const input = fs.readFileSync(path.resolve(__dirname, "../input.txt")).toString().split('\r\n')
+const input = fs.readFileSync(path.resolve(__dirname, "../input.txt")).toString().split('\r\n').map(i => i.split(''));
 
 module.exports = {
     run: () => {
-        //for (const row of input) {
-
-        //}
         return 'todo';
     }
 }
--- 2023/index.js
+++ 2023/index.js
@@ -30,19 +30,27 @@ const config = {
 
 readdirSync('./2023').filter(n => n.includes('Day')).forEach((i, index) => {
     const day = require(`./${i}/index.js`).run();
+
+    if (process.argv[3] && process.argv[3] === 'skip') {
+        return;
+    }
+
     if (!process.argv[2] || process.argv[2] !== 'true') {
         day.partOne.result = '|'.repeat(day.partOne.result.toString().length);
         day.partTwo.result = '|'.repeat(day.partTwo.result.toString().length);
     }
 
-    const partOneTook = Number(day.partOne.took).toFixed(2) + 'ms';
-    const cPartOneTook = Number(day.partOne.took).toFixed(2) > 10 ? red(partOneTook) : green(partOneTook);
-    const partTwoTook = Number(day.partTwo.took).toFixed(2) + 'ms';
-    const cPartTwoTook = Number(day.partTwo.took).toFixed(2) > 10 ? red(partTwoTook) : green(partTwoTook);
+    const partOneTook = day.partOne.result === 'todo' ? '-' : Number(day.partOne.took).toFixed(2) + 'ms';
+    const cPartOneTook = day.partOne.result === 'todo' ? red(partOneTook) : Number(day.partOne.took).toFixed(2) > 10 ? red(partOneTook) : green(partOneTook);
+    const partTwoTook = day.partTwo.result === 'todo' ? '-' : Number(day.partTwo.took).toFixed(2) + 'ms';
+    const cPartTwoTook = day.partTwo.result === 'todo' ? red(partTwoTook) : Number(day.partTwo.took).toFixed(2) > 10 ? red(partTwoTook) : green(partTwoTook);
     const took = Number(day.took).toFixed(2) + 'ms';
     const cTook = Number(day.took).toFixed(2) > 10 ? red(took) : green(took);
 
-    data.push([index % 2 === 0 ? r(i.split(' ')[1]) : g(i.split(' ')[1]), day.desc, day.partOne.result, cPartOneTook, day.partTwo.result, cPartTwoTook, cTook]);
+    data.push([index % 2 === 0 ? r(i.split(' ')[1]) : g(i.split(' ')[1]), [day.partOne.result, day.partTwo.result].includes('todo') ? red(day.desc) : green(day.desc), day.partOne.result === 'todo' ? red(day.partOne.result) : day.partOne.result, cPartOneTook, day.partTwo.result === 'todo' ? red(day.partTwo.result) : day.partTwo.result, cPartTwoTook, [day.partOne.result, day.partTwo.result].includes('todo') ? red('-') : cTook]);
 })
 
+if (process.argv[3] && process.argv[3] === 'skip') {
+    return;
+}
 console.log(table(data, config));
